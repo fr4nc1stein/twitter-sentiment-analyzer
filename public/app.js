57b2959944c3a812a4c0b23de2f20ee8a90cc247
Al Francis <fr4nc1stein@gmail.com>
2019-12-02 19:24:35 +0800

    push

---
+++ public/app.js
@@ -0,0 +1,116 @@
+Vue.config.productionTip = false;
+new Vue({
+    el: '#app',
+    
+    vuetify: new Vuetify(),
+    delimiters: ['<%', '%>'],
+    created () {this.query = 'Donald Trump';
+		// this.search();
+	},
+    mixins: [Vue2Filters.mixin],
+	data: () => ({
+		title: null,
+		query: null,
+		statuses: [],
+		tab: null,
+		loading: false
+    }),
+    
+    methods: {
+		async search () {
+			this.loading = true;
+			try {
+                const res = await fetch(`/search/${this.query}`);
+                const data = await res.json();
+				this.title = this.query;
+
+				if (data.success) {
+					this.statuses = data.data;
+				}
+			} catch (error) {
+				console.log(error);
+			}
+			this.loading = false;
+		}
+    },
+    computed: {
+		pieChartData () {
+			const positives = this.statuses.filter(status => status.sentiment.score > 0).length;
+			const negatives = this.statuses.filter(status => status.sentiment.score < 0).length;
+
+			const datasets = [
+			{
+				label: 'Total',
+				backgroundColor: ['#2196F3', '#F44336'],
+				data: [positives, negatives]
+			}
+			];
+
+			return {
+				labels: ['Positive Tweets', 'Negative Tweets'],
+				datasets: datasets
+			}
+		},
+
+		barChartData () {
+			const positives = [].concat.apply([], this.statuses.map((status) => {
+				return status.sentiment.positive;
+			})).length;
+
+			const negatives = [].concat.apply([], this.statuses.map((status) => {
+				return status.sentiment.negative;
+			})).length;
+
+			// const labels = Array.from(new Set(words));
+
+			const datasets = [
+			{
+				label: 'Positive Vs Negative Words',
+				backgroundColor: ['#2196F3', '#F44336'],
+				data: [positives, negatives]
+			}
+			];
+
+			return {
+				labels: ['Positive', 'Negative'],
+				datasets: datasets
+			}
+		},
+
+		latest () {
+			return this.statuses.sort((a, b) => parseFloat(a.timestamp) - parseFloat(b.timestamp));
+		}
+	},
+
+	components: {
+		'pie-chart': {
+			extends: VueChartJs.Pie,
+			mixins: [VueChartJs.mixins.reactiveProp],
+			props: ['chartData'],
+			mounted () {
+				console.log('Pie Chart Connected');
+				this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false});
+			}
+		},
+		'bar-chart': {
+			extends: VueChartJs.Bar,
+			mixins: [VueChartJs.mixins.reactiveProp],
+			props: ['chartData'],
+			mounted () {
+				console.log('Bar Chart Connected');
+				this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false});
+			}
+		}
+	},
+
+	filters: {
+		moment (val) {
+			return moment(val, 'ddd MMM DD HH:mm:ss Z YYYY').format('MMMM D, YYYY, h:mm:ss a');
+        },
+        capitalize: function (value) {
+            if (!value) return ''
+            value = value.toString()
+            return value.charAt(0).toUpperCase() + value.slice(1)
+          }
+	}
+  })
